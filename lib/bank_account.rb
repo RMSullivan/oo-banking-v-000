95c26547f43157612a05b00ac3e3de11a4af1d12
Rita Sullivan <ritarojas@mac.com>
2018-07-08 17:38:44 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,15 @@
 class BankAccount
+attr_reader :name
+attr_accsor :balance, :status
 
+def initialize(name)
+  @mane = name
+  @balance = 1000
+  @status = "open"
+end
+def deopsit(deposit_amount)
+  self.balance +=deposit_amount
+end
+
+  
 end
